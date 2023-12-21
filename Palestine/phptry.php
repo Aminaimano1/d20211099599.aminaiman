cba8e6bbaf1209b59d31dd580e911dfd200568d7
Aminaimano1 <133790772+Aminaimano1@users.noreply.github.com>
2023-12-21 23:11:24 +0800

    Add files via upload

---
+++ Palestine/phptry.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My First PHP program</title>
+        <body>
+            <?php
+                echo "Hello World";
+            ?>
+        </body>
+    </head>
+</html>
---
+++ Palestine/welcomewq.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+<form action = "" method="post">
+<p><input type = "text" name = "name" id="name" placeholder = "enter your name"></p>
+<p><input type = "text" name = "matrix" id="matrix" placeholder = "enter your matrix number"></p>
+    <p>
+        <input type = "submit" value = "Submit" name="submit">
+        <input type = "submit" value = "Reset" name="reset">
+    </p>
+    </form>
+
+<?php
+if (isset($_POST["submit"])){
+    //retrieve
+    $name = $_POST["name"];
+    $matrik = $_POST["matrix"];
+    ?>
+    Welcome to my page : <?php echo $name;  ?>
+
+<?php
+}
+?>
+
+</body>
+</html>
